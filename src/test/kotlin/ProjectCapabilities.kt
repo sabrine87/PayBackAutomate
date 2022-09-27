c2e0cc9fd3ed261a99ba01f1f755adcb299f9145
smo <smo@ip-label.com>
2022-09-27 23:41:56 +0100

    Add project capabilities

--- src/test/kotlin/ProjectCapabilities.kt
+++ src/test/kotlin/ProjectCapabilities.kt
@@ -4,7 +4,14 @@ class ProjectCapabilities {
     companion object {
         fun AndroidBaseCapabilities(): DesiredCapabilities {
             val caps = DesiredCapabilities()
-
+            caps.setCapability("appium:fullReset", false)
+            caps.setCapability("appium:noReset", true)
+            caps.setCapability("platformName", "Android")
+            caps.setCapability("appium:automationName", "UiAutomator2")
+            caps.setCapability("appium:deviceName", "Galaxy S8")
+            caps.setCapability("appium:udid", "9889d633594e584853")
+            caps.setCapability("appium:appPackage", "de.payback.client.android")
+            caps.setCapability("appium:appActivity", "de.payback.app.deeplinks.StarterActivity")
             return caps
         }
     }
