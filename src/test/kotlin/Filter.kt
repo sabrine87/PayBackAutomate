c55a533e367fabc2d41c31ea6cbd7c07fafbdcab
smo <smo@ip-label.com>
2022-09-27 23:43:18 +0100

    Add Action to Coupon button and Filter button

--- src/test/kotlin/Filter.kt
+++ src/test/kotlin/Filter.kt
@@ -1,6 +1,16 @@
 import com.tngtech.jgiven.Stage
-
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.android.AndroidDriver
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 
 open class Filter : Stage<Filter?>() {
-
+    fun go_to_filter_page(driver: AppiumDriver?, wait: WebDriverWait): Filter? {
+        driver?.findElement(
+            By.id(
+                "de.payback.client.android:id/coupon_center_dest"))?.click()
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("de.payback.client.android:id/filter_button"))).click()
+        return self()
+    }
 }
