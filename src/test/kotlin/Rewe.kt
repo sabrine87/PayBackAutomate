87965dc8d3a281601efc157b0a9d2f99dbb06683
smo <smo@ip-label.com>
2022-09-27 23:43:55 +0100

    Wait until REWE is displayed and filter by partner REWE ( click on REWE)

--- src/test/kotlin/Rewe.kt
+++ src/test/kotlin/Rewe.kt
@@ -1,6 +1,17 @@
 import com.tngtech.jgiven.Stage
-
+import io.appium.java_client.AppiumDriver
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 
 open class Rewe : Stage<Rewe?>() {
-
+    fun go_to_rewe(driver: AppiumDriver?, wait: WebDriverWait): Rewe? {
+        wait.until(
+            ExpectedConditions.presenceOfElementLocated
+                (By.id("de.payback.client.android:id/tile"))).isDisplayed
+        driver?.findElements(
+            By.id(
+                "de.payback.client.android:id/tile"))?.get(1)?.click()
+        return self()
+    }
 }
